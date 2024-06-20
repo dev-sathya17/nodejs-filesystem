808a30980665148eb20a90a8d059413904771af1
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-06-20 15:53:27 +0530

    completed creating files on POST call

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+// Importing required packages.
 const express = require("express");
+const fs = require("fs");
 
+// Creating an express app.
 const app = express();
 
-app.get("/", (request, reponse) => {
-  reponse.send("Hello World");
+// Creating the date Object to work with date n time.
+const dateObj = new Date();
+
+// Getting date values and formatting them into a single string.
+const date = `${dateObj.getDate()}-${
+  dateObj.getMonth() + 1
+}-${dateObj.getFullYear()}`;
+
+// Getting time values and formatting them into a single string.
+const time = `${dateObj.getHours()}-${dateObj.getMinutes()}-${dateObj.getSeconds()}`;
+
+// Creating a data to be inserted within the file.
+const data = `Current Timestamp: ${date.replaceAll("-", "/")}-${time.replaceAll(
+  "-",
+  ":"
+)}`;
+
+// Creating a POST route to create a file.
+app.post("/files", (request, response) => {
+  fs.writeFile(`./files/${date}-${time}.txt`, data, (err) => {
+    // Checking for any errors.
+    if (err) {
+      response.send("Error while creating file", err);
+    } else {
+      response.send("File created successfully");
+    }
+  });
 });
 
 app.listen(3000, () => {
